dec7cac1c5fb12f237d508f1c819c898be45b9aa
Saajan <saajan.sn@gmail.com>
2020-03-20 04:24:36 +0530

    chalk added

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
-var os = require("os");
+const os = require("os");
+const chalk = require('chalk');
+const log = console.log;
 
-console.log("--- OS DETAILS ---");
-console.log("OS HOSTNAME :",os.hostname());
-console.log("OS TYPE :",os.type());
-console.log("OS PLATFORM :",os.platform());
-console.log("OS ARCHITECTURE :",os.arch());
-console.log("OS RELEASE :",os.release());
-console.log("CONSOLE PROFILE : ",process.env.KONSOLE_PROFILE_NAME);
-console.log("DESKTOP SESSION :",process.env.DESKTOP_SESSION);
-console.log("USERNAME :",process.env.USERNAME);
-console.log("LANGUAGE : ",process.env.LANGUAGE);
-
-console.log("--- NETWORK ---");
-console.log("NETWORK :",os.networkInterfaces());
+log(chalk.blue("--- OS DETAILS ---"));
+log(chalk.green("OS HOSTNAME :"), os.hostname());
+log(chalk.green("OS TYPE :"), os.type());
+log(chalk.green("OS PLATFORM :"), os.platform());
+log(chalk.green("OS ARCHITECTURE :"), os.arch());
+log(chalk.green("OS RELEASE :"), os.release());
+log(chalk.green("UPTIME :"), os.uptime());
 
 
-console.log("--- CPU DETAILS ---");
-console.log("CPU :",os.cpus()[0].model);
-console.log("CPU CORES :",os.cpus().length);
+log(chalk.blue("--- CPU DETAILS ---"));
+log(chalk.green("CPU :"), os.cpus()[0].model);
+log(chalk.green("CPU CORES :"), os.cpus().length);
 
 
-console.log("--- MEMORY --");
-console.log("UPTIME :",os.uptime());
-console.log("FREE MEMORY : ",os.freemem());
-console.log( os.endianness());
+log(chalk.blue("--- MEMORY --"));
+log(chalk.green("TOTAL MEMORY :"), os.totalmem());
+log(chalk.green("FREE MEMORY :"), os.freemem());
+log(chalk.green(os.endianness()));
 
-console.log("---PROCESS DETAILS---");
-console.log("COMMAND : ",process.title);
-console.log("NODE VERSION : ",process.version);
-console.log("NODE ARGUMENTS : ",process.argv);
-console.log("HOME :",process.env.HOME);
-console.log("LOGNAME : ",process.env.LOGNAME);
-console.log("PRESENT WORKING DIRECTORY :",process.env.PWD);
-console.log("AGENT PID :",process.env.SSH_AGENT_PID);
+log(chalk.blue("---PROCESS DETAILS---"));
+log(chalk.green("COMMAND :"), process.title);
+log(chalk.green("NODE VERSION :"), process.version);
+log(chalk.green("NODE ARGUMENTS :"), process.argv);
+log(chalk.green("HOME :"), process.env.HOME , os.homedir());
+log(chalk.green("LOGNAME :"), process.env.LOGNAME);
+log(chalk.green("PRESENT WORKING DIRECTORY :"), process.env.PWD);
+log(chalk.green("AGENT PID :"), process.env.SSH_AGENT_PID);
+
+log(chalk.blue("--- NETWORK ---"));
+log("NETWORK :", os.networkInterfaces());
